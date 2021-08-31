fe9ac0a76c1bce7d83053a7b950f1cca1b563838
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-09-01 12:43:54 +1300

    created employee page with responsive design

--- assets/js/employee.js
+++ assets/js/employee.js
@@ -50,25 +50,3 @@ clickBig.addEventListener('click', function(){
     openBig.classList.toggle('active');
     menuIconBig.classList.toggle('active');
 })
-
-
-// tab list
-const testimonialsPersons = Array.from(document.querySelectorAll('.testimonials-persons a'));
-const personsTexts = Array.from(document.querySelectorAll('.person-text'));
-const clearActives = () => {
-    testimonialsPersons.forEach(testimonialsPerson => {
-        testimonialsPerson.classList.remove('active');
-    });
-    personsTexts.forEach(personsText => {
-        personsText.classList.remove('active');
-    });
-}
-testimonialsPersons.forEach(testimonialsPerson => {
-    testimonialsPerson.onclick = function () {
-        clearActives();
-        const targetId = testimonialsPerson.getAttribute('data-target')
-        const targetContent = document.getElementById(targetId);
-        testimonialsPerson.classList.add('active');
-        targetContent.classList.add('active');
-    }
-});
